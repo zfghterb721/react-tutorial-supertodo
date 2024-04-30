fe2cc97efe64068285158aee255b6a2b1a86dbe8
Barrett Routon <barrett@binderpos.com>
2024-04-30 09:56:52 -0500

    Added a basic react hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const rulesText = [
+  "Use className instead of class in element attributes",
+  "All tags MUST be closed (have their pair somewhere), you can shorthand a closed pair like so <br/>",
+  "This code is expected to run in the browser* we just use node to compile it. It's javascript but it's not node. You can't install any library but you can many.",
+  "*Server side rendering (like in nextjs) will let us combine the two AND sort of use backend stuff and frontend stuff in the same project",
+];
+
+const SuperListItem = (props) => {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <>
+      <li>
+        {checked?<s>{props.itemText}</s>:props.itemText} 
+        <input
+          type="checkbox"
+          value={checked}
+          onChange={() => {
+            setChecked(!checked);
+          }}
+        ></input>
+      </li>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       This is just regular HTML except you can throw in js when you want:
-      <br/>
-      {new Date().toISOString(0)}
-      <br/>
+      <br />
+      {new Date().toISOString()}
+      <br />
       Some extra rules:
       <ul>
-        <li>Use className instead of class in element attributes</li>
-        <li>All tags MUST be closed (have their pair somewhere), you can shorthand a closed pair like so {"<br/>"}</li>
-        <li>This code is expected to run in the browser* we just use node to compile it. It's javascript but it's not node. You can't install any library but you can many.</li>
-        <li>*Server side rendering (like in nextjs) will let us combine the two AND sort of use backend stuff and frontend stuff in the same project</li>
+        {rulesText.map((r) => (
+          <SuperListItem itemText={r} />
+        ))}
       </ul>
     </div>
   );
