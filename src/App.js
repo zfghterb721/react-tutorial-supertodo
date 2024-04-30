790103f62e2b4bdc32b34eec5d07c2a8235069f0
Barrett Routon <barrett@binderpos.com>
2024-04-30 08:49:16 -0500

    Boiled down React starter project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is just regular HTML except you can throw in js when you want:
+      <br/>
+      {new Date().toISOString(0)}
+      <br/>
+      Some extra rules:
+      <ul>
+        <li>Use className instead of class in element attributes</li>
+        <li>All tags MUST be closed (have their pair somewhere), you can shorthand a closed pair like so {"<br/>"}</li>
+        <li>This code is expected to run in the browser* we just use node to compile it. It's javascript but it's not node. You can't install any library but you can many.</li>
+        <li>*Server side rendering (like in nextjs) will let us combine the two AND sort of use backend stuff and frontend stuff in the same project</li>
+      </ul>
     </div>
   );
 }
